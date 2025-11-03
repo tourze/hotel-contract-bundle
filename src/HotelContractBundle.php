a943eff3a57ee0807fd8e6e72a737c46387974f9
github-ci <you@example.com>
2025-11-03 14:20:38 +0800

    update++++++++++++

--- src/HotelContractBundle.php
+++ src/HotelContractBundle.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
 use Tourze\DoctrineTimestampBundle\DoctrineTimestampBundle;
 use Tourze\DoctrineUserBundle\DoctrineUserBundle;
+use Tourze\EasyAdminMenuBundle\EasyAdminMenuBundle;
 use Tourze\EnvManageBundle\EnvManageBundle;
 use Tourze\HotelProfileBundle\HotelProfileBundle;
 use Tourze\RoutingAutoLoaderBundle\RoutingAutoLoaderBundle;
@@ -30,6 +31,7 @@ class HotelContractBundle extends Bundle implements BundleDependencyInterface
             EnvManageBundle::class => ['all' => true],
             DoctrineTimestampBundle::class => ['all' => true],
             DoctrineUserBundle::class => ['all' => true],
+            EasyAdminMenuBundle::class => ['all' => true],
         ];
     }
 }
